648e5fe17c5c582a79f21a430b35f46e205b3b9e
Oleg Klimov <o1egkl@gmail.com>
2023-08-22 13:13:02 +0300

    2- commit

--- calculation2.sh
+++ calculation2.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
 function display_max(){
-
+ echo "display_max\n"
 }
 function display_avg(){
-    
+    echo "display_avg\n"
 }
 function display_min(){
-    
+    echo "display_min\n"
 }
 function display_count(){
-    
+    echo "display_count\n"
 }
 function display_sum(){
-    
+    echo "display_sum\n"
 }
