a354dedc127d5f00c6644869dbd35f8f7e7a491b
Oleg Klimov <o1egkl@gmail.com>
2023-08-22 13:10:15 +0300

    1- st commit

---
+++ calculation2.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+function display_max(){
+
+}
+function display_avg(){
+    
+}
+function display_min(){
+    
+}
+function display_count(){
+    
+}
+function display_sum(){
+    
+}
